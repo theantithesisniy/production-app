a2568d2c2ac93ad32109d4f8c9dc6ffaeb3f5dbd
theantithesis <theantithesis03@gmail.com>
2024-08-22 17:07:00 +0300

    configured react, scss, css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,32 @@
-import webpack from "webpack";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')), //only module.scss gen hash
+            localIdentName: isDev
+              ? '[path][name]__[local]--[hash:base64:5]'
+              : '[hash: base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
 
-    return [
-        typescriptLoader,
-    ]
+  return [typescriptLoader, cssLoaders];
 }
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { Counter } from './Counter'
+import '../index.scss';
+
+const App = () => {
+	return (
+		<div className='app'>
+			asdasd
+			<Counter/>
+		</div>
+	)
+}
+
+
+export default App
