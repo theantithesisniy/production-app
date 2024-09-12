10bc7cd0e042979eb313244c3ccfcfff52fa6adb
theantithesis <theantithesis03@gmail.com>
2024-09-12 19:09:45 +0300

    organizing styles and adding a theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import '../index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MailPage.async';
+import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './components/pages/MainPage/MailPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+	const { theme, toggleTheme } = useTheme()
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Сменить тему</button>
 			<Link to={'/'}> Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { render } from 'react-dom';
-import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import ThemeProvider from './theme/ThemeProvider';
 
-render (
-	<BrowserRouter><App/></BrowserRouter>,
+render(
+	<BrowserRouter>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</BrowserRouter>,
 	document.getElementById('root')
 )  
